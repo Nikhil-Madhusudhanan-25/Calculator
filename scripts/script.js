fb15e69b4c050cd02a3fedbd24f8041bd9a7f10a
nikhil-madhusudhanan-25 <nikhilmadhusudhanan20@gmail.com>
2024-01-09 19:01:52 +0530

    Create a function that calls the math functions based on user input

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ function add(num1, num2){
     return num1+num2;
 }
 function subtract(num1, num2){
-    return num1-num2;
+    return num1-num2    ;
 }
 function multiply(num1, num2){
     return num1*num2;
@@ -12,4 +12,19 @@ function divide(num1, num2){
 }
 let num1=0,
     num2=0,
-    operator="";
+    operator="",
+    result=0;
+function operate(a, b, op)
+{
+        switch(op){
+            case '+': result=add(+a,+b);
+            break;
+            case '-': result=subtract(+a,+b);
+            break;
+            case '*': result=multiply(+a, +b);
+            break;
+            case '/': result=divide(+a, +b);
+            break;
+            default: console.log("Invalid operation");
+        }
+    }
